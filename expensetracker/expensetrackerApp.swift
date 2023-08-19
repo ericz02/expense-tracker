a195488f9565cd7a9d4de7542336d74a34f75664
ericz02 <ezheng0720@gmail.com>
2023-08-18 22:33:39 -0400

    Initial Commit

---
+++ expensetracker/expensetrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensetrackerApp.swift
+//  expensetracker
+//
+//  Created by Eric Zheng on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct expensetrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
